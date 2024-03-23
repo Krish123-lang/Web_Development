4c700969ff6e40e20bf2dfb44d398bb3db96b770
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-23 21:53:58 +0545

    Js strings

---
+++ JavaScript/Basic/Programs/first/first.js
@@ -0,0 +1,6 @@
+let num1 = 20;
+let num2 = 45;
+
+
+console.log(`The sum of ${num1} and ${num2} is: ${num1 + num2} `);
+
---
+++ JavaScript/Basic/Programs/first/strings.js
@@ -0,0 +1,33 @@
+// function mySum(a,b){
+//     return a+b;
+// }
+
+// s=mySum(5,2)
+// console.log(s)
+
+// ----------------------------------
+
+// length 
+let a="this is krishna";
+console.log(a.length); // 15
+
+// ToUpperCase()
+console.log(a.toUpperCase()); // THIS IS KRISHNA
+
+
+// ToLowerCase()
+console.log(a.toLowerCase()); // this is krishna
+
+// -----------------------------------------
+
+function getCharCount(str) {
+    return str.length;
+    return str.toUpperCase();
+    return str.toLowerCase();
+}
+
+// Sample usage - do not modify
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
+console.log(getCharCount("Charley is here")); // 15
+
