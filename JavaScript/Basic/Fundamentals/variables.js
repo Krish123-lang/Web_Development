37d8c922a2a6a4b9c97bb072d9bf2e7825839264
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-23 23:17:39 +0545

    Create variables.js

---
+++ JavaScript/Basic/Fundamentals/variables.js
@@ -0,0 +1,43 @@
+// let message;
+// message = "hello world";
+
+// let message = "hello world";
+// console.log(message); // hello world
+
+// --------- NOT RECOMMENDED ----------
+// let user = 'John', age = 25, message = 'Hello';
+// console.log(user, age, message); // John 25 Hello
+
+// -----------------------------------
+
+/*
+let name = "Apple";
+console.log(name);
+
+name = "ball";
+console.log(name);
+*/
+
+/*
+Apple
+ball
+*/
+
+// -----------------------------------
+
+/*
+let name = "hello";
+let anime;
+
+anime = name;
+console.log(name, anime); // hello hello
+*/
+
+const myBirthday = '18.04.1982';
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+console.log(color); // #FF7F00
