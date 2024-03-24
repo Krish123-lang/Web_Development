c58ec8ca01c4aa667a2605889994235f48121d6b
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-24 12:09:45 +0545

    Create operators.js

---
+++ JavaScript/Basic/Fundamentals/operators.js
@@ -0,0 +1,101 @@
+// *** ARITHMETIC OPS ***
+
+// let a = 10;
+// let b = 5;
+
+// let c = 11;
+// let d = 3;
+
+/*
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a ** b); // 100000
+console.log(c % d); // remainder = 2
+*/
+
+// -------------------------------------------
+
+// let i = 5;
+
+// console.log(++i); // 6 => adds 1 and prints on same time
+// console.log(i); // 6
+// console.log(i++); // 5 => adds and store result within itself
+// console.log(i); // 6 => Prints the result
+// console.log(--i); // 4 => minus 1 and prints on same time
+// console.log(i); // 4
+// console.log(i--); // 5 => minus 1 and store result within itself
+// console.log(i); // 4 => Prints the result
+// =========================================================
+
+// *** ASSIGNMENT OPS ***
+// let a = 10;
+// let b = 5;
+
+/*
+console.log(a); // 10
+console.log(b); // 5
+
+a = b;
+
+console.log(a); // 5
+console.log(b); // 5
+
+b = a;
+
+console.log(a); // 10
+console.log(b); // 10
+*/
+
+// console.log(a += b); // 15
+// console.log(a -= b); // 5
+// console.log(a *= b); // 50
+// console.log(a /= b); // 2
+// console.log(a %= b); // 0
+// console.log(a **= b); // 100000
+
+// -------------------------------------------------------
+
+// *** COMPARISION OPS ***
+
+/*
+let a = 5;
+let b = 10;
+let c = 5;
+
+console.log(a == b); // false
+console.log(a == c); // true
+
+// === => equal value and equal data type
+console.log(a === c); // true
+console.log(a === b); // false
+
+console.log(a != b); // true
+console.log(a !== c); // false
+
+console.log(a < b); // true
+console.log(a > b); // false
+
+console.log(a <= b); // true
+console.log(a >= b); // false
+
+console.log((a < b) ? "hello" : "Bye"); // hello
+console.log((b < a) ? "hello" : "Bye"); // Bye
+*/
+
+/*
+let a = "20";
+let b = "5";
+console.log(a < b); // true
+console.log(a > b); // false
+
+console.log(a + " " + b); // 20 5
+
+let a = "hello";
+console.log(a + 6); // hello6
+*/
+
+// ==========================================================
+
+// *** LOGICAL OPS (&&, ||, !) ***
