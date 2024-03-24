5be7b688d22ae6ef842f912efb2c3d4119490015
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-24 19:29:22 +0545

    Create strings.js

---
+++ JavaScript/Basic/Fundamentals/strings.js
@@ -0,0 +1,13 @@
+let name="krishna"
+
+console.log(name.length); // 7
+
+console.log(name.toUpperCase()); // KRISHNA
+
+console.log(name.toLowerCase()); // krishna
+
+console.log(name.slice(2,4)); // is
+
+console.log(name.slice(2)); // ishna
+
+console.log(name.replace("s", "j")); // krijhna
